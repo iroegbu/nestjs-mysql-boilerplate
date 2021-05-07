d2e758004a371c79daeb2133d460fd77a127d944
Iroegbu <iroegbu@Nnabuezes-MacBook-Pro.local>
2021-05-07 13:02:06 +0100

    chore(database): include packages for database integration
    - typeorm orm for connection to database and model management
    - class-validation for validating dtos
    - class-transform to transform requests to dto and other changes.

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,9 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { HomeModule } from './home/home.module';
-import { UserModule } from './user/user.module';
 import { UserModule } from './user/user.module';
 
 @Module({
-  imports: [HomeModule, UserModule],
+  imports: [HomeModule, UserModule, TypeOrmModule.forRoot()],
 })
 export class AppModule {}
