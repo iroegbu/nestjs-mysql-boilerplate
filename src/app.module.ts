35e8079089609505fe28e75b4be024ca32d882d4
Iroegbu <iroegbu@Nnabuezes-MacBook-Pro.local>
2021-05-07 11:26:05 +0100

    feature(user): user module
    initialize user module with `nest g res user`.

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,9 @@
 import { Module } from '@nestjs/common';
 import { HomeModule } from './home/home.module';
+import { UserModule } from './user/user.module';
+import { UserModule } from './user/user.module';
 
 @Module({
-  imports: [HomeModule],
+  imports: [HomeModule, UserModule],
 })
 export class AppModule {}
