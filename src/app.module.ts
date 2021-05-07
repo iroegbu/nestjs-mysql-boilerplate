c97ddb205eaab57752a608efe356955331df3cd6
Iroegbu <iroegbu@Nnabuezes-MacBook-Pro.local>
2021-05-07 16:24:39 +0100

    feat(config): include config for connecting to database

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,25 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import databaseConfig from 'config/databaseConfiguration';
+import { getConnectionOptions } from 'typeorm';
 import { HomeModule } from './home/home.module';
 import { UserModule } from './user/user.module';
 
 @Module({
-  imports: [HomeModule, UserModule, TypeOrmModule.forRoot()],
+  imports: [
+    HomeModule,
+    UserModule,
+    ConfigModule.forRoot({ load: [databaseConfig], isGlobal: true }),
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: databaseConfig().host,
+      port: databaseConfig().port,
+      username: databaseConfig().username,
+      password: databaseConfig().password,
+      database: databaseConfig().database,
+      entities: databaseConfig().entities,
+    }),
+  ],
 })
 export class AppModule {}
--- src/user/entities/user.entity.ts
+++ src/user/entities/user.entity.ts
@@ -1,1 +1,13 @@
-export class User {}
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity()
+export class User {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column()
+  nick: string;
+
+  @Column({ default: Date.now() })
+  dateCreated: Date;
+}
