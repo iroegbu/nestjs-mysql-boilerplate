e2b117681690a427f41a596c963d8104d7a109be
Iroegbu <iroegbu@Nnabuezes-MacBook-Pro.local>
2021-05-11 14:43:21 +0100

    refactor(module): removes user module and migration directory

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,13 +2,11 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { configService } from './config/config.service';
 import { HomeModule } from './home/home.module';
-import { UsersModule } from './users/users.module';
 
 @Module({
   imports: [
     HomeModule,
     TypeOrmModule.forRoot(configService.getTypeOrmConfig()),
-    UsersModule,
   ],
 })
 export class AppModule {}
